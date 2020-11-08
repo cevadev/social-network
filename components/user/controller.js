b4b739d6e6d06f57b83be407606f65c9a5de23d1
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-11-08 19:33:17 -0300

    Curso practico Nodejs

--- components/user/controller.js
+++ components/user/controller.js
@@ -1,12 +1,19 @@
-const store = require('../../store/dummy.js');
-
 const TABLA = 'user';
 
-function list(){
-    //retornamos los usuarios
-    return store.list(TABLA);
-}
-
-module.exports = {
-    list,
+/** exportamos un funcion que trabaje con el store inyectado y no con el definido en el archivo */
+module.exports = function(injectedStore){
+    
+    //nuestra funcion list() ahora trabaja con el store inyectado
+    function list(){
+        let store = injectedStore;
+        if(!store){
+            store = require('../../store/dummy.js');
+        }
+        //retornamos los usuarios
+        return store.list(TABLA);
+}   
+    return{
+        //retornamos la funcion list
+        list,
+    }
 }
---
+++ components/user/index.js
@@ -0,0 +1,10 @@
+/**este archivo se encarga de realizar toda la importacion de los controladores. */
+
+//importamos la bd que queremos configurar por defecto para nuestro controlador
+const store = require('../../store/dummy.js');
+const controller = require('./controller.js');
+
+/**convertimos al objeto controller en una funcion 
+ * le inyectamos el store
+*/
+module.exports = controller(store);
